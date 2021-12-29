519478bdab2eb3e66aa7a786b31a0977182d040e
devalio <github@devalio.ru>
2021-12-29 20:55:15 +0300

    test results with reduced `maxLoggedCount` from 100 to 5

--- loginCounter/array_buckets_test.go
+++ loginCounter/array_buckets_test.go
@@ -6,7 +6,7 @@ func Benchmark_counter_arrayBucketLogger(b *testing.B) {
 	/*
 		cpu: Intel(R) Core(TM) i7-9700F CPU @ 3.00GHz
 		Benchmark_counter_arrayBucketLogger
-		Benchmark_counter_arrayBucketLogger-8   	 1000000	    126586 ns/op	    9224 B/op	       3 allocs/op
+		Benchmark_counter_arrayBucketLogger-8   	  383007	    197112 ns/op	   47239 B/op	      22 allocs/op
 	*/
 	var c = NewArrayBucketLogger()
 	benchCounter(b, c)
--- loginCounter/map_buckets_test.go
+++ loginCounter/map_buckets_test.go
@@ -6,7 +6,7 @@ func Benchmark_counter_mapBucketLogger(b *testing.B) {
 	/*
 		cpu: Intel(R) Core(TM) i7-9700F CPU @ 3.00GHz
 		Benchmark_counter_mapBucketLogger
-		Benchmark_counter_mapBucketLogger-8   	  779850	     13804 ns/op	    9050 B/op	       3 allocs/op
+		Benchmark_counter_mapBucketLogger-8     	  643489	     44557 ns/op	   66298 B/op	      36 allocs/op
 	*/
 	var c = NewMapBucketLogger()
 	benchCounter(b, c)

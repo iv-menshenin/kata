c536ac3c3d9844a280fd1684ab3fa43609769cd2
devalio <github@devalio.ru>
2022-01-03 06:45:20 +0300

    matrix.go refactored

--- dijkstra/matrix.go
+++ dijkstra/matrix.go
@@ -5,9 +5,9 @@ type (
 		geo [][]int
 	}
 	matrixExplorer struct {
-		explored []bool
-		current  []int
-		paths    [][]int
+		explored        []bool
+		exploredWeights []int
+		exploredPaths   [][]int
 	}
 )
 
@@ -17,78 +17,69 @@ const (
 )
 
 func (p *matrixGraph) GetPath(from, to int) (int, []int) {
-	var explorer = matrixExplorer{
-		explored: make([]bool, len(p.geo)),
-		paths:    make([][]int, len(p.geo)),
-	}
-	for i := 0; i < len(explorer.paths); i++ {
-		explorer.paths[i] = make([]int, 0, len(p.geo))
-	}
-	var current = from
-	var currentCost = 0
-	var canContinue bool
+	var (
+		current     = from
+		canContinue bool
+		explorer    = newExplorer(len(p.geo))
+	)
 	for {
-		explorer.explored[current] = true
-		exploration := explorer.explorePath(p.geo[current])
-		explorer.mergeExplorations(exploration, currentCost, current)
-		current, currentCost, canContinue = explorer.nextNode()
-		if !canContinue {
+		explorer.step(p.geo[current], current)
+		if current, canContinue = explorer.nextNode(); !canContinue {
 			break
 		}
 	}
-	if to < len(explorer.current) {
-		return explorer.current[to], explorer.paths[to]
+	if to < len(explorer.exploredPaths) && len(explorer.exploredPaths[to]) > 0 {
+		return explorer.exploredWeights[to], explorer.exploredPaths[to]
 	}
 	return infinity, nil
 }
 
-func (e *matrixExplorer) mergeExplorations(bestPaths []int, currentCost, currentIdx int) {
-	if len(e.current) == 0 {
-		for i := 0; i < len(bestPaths); i++ {
-			if bestPaths[i] != infinity {
-				e.paths[i] = append(e.paths[i], []int{currentIdx, i}...)
-			}
-			bestPaths[i] += currentCost
-		}
-		e.current = bestPaths
-		return
+func newExplorer(l int) *matrixExplorer {
+	var explorer = matrixExplorer{
+		explored:        make([]bool, l),
+		exploredPaths:   make([][]int, l),
+		exploredWeights: make([]int, l),
 	}
-	for i := 0; i < len(bestPaths); i++ {
-		if bestPaths[i] == infinity {
-			continue
+	for i := 0; i < len(explorer.exploredPaths); i++ {
+		explorer.exploredPaths[i] = make([]int, 0, l)
+	}
+	return &explorer
+}
+
+func (e *matrixExplorer) analysePathTo(currentNodeIdx, targetNodeIdx, weight int) {
+	currentNodePathWeight := e.exploredWeights[currentNodeIdx]
+	if e.exploredWeights[targetNodeIdx] == uncovered || e.exploredWeights[targetNodeIdx] > weight+currentNodePathWeight {
+		emptyBuff := e.exploredPaths[targetNodeIdx][:0]
+		if len(e.exploredPaths[currentNodeIdx]) > 0 {
+			e.exploredPaths[targetNodeIdx] = append(append(emptyBuff, e.exploredPaths[currentNodeIdx]...), targetNodeIdx)
+		} else {
+			e.exploredPaths[targetNodeIdx] = append(emptyBuff, currentNodeIdx, targetNodeIdx)
 		}
-		if e.current[i] == infinity || e.current[i] > bestPaths[i]+currentCost {
-			e.paths[i] = append(append(e.paths[i][:0], e.paths[currentIdx]...), i)
-			e.current[i] = bestPaths[i] + currentCost
-		}
+		e.exploredWeights[targetNodeIdx] = weight + currentNodePathWeight
 	}
 }
 
-func (e *matrixExplorer) nextNode() (int, int, bool) {
-	const notFound = -1
-	var node, weight = notFound, infinity
-	for i := 0; i < len(e.current); i++ {
-		if e.explored[i] || e.current[i] == infinity {
+func (e *matrixExplorer) step(node []int, currentNodeIdx int) {
+	e.explored[currentNodeIdx] = true
+	for i, w := range node {
+		if w == uncovered {
 			continue
 		}
-		if e.current[i] < weight || node < 0 {
-			weight = e.current[i]
+		e.analysePathTo(currentNodeIdx, i, w)
+	}
+}
+
+func (e *matrixExplorer) nextNode() (int, bool) {
+	var node = -1
+	for i, w := range e.exploredWeights {
+		if e.explored[i] || w == uncovered {
+			continue
+		}
+		if node < 0 || w < e.exploredWeights[node] {
 			node = i
 		}
 	}
-	return node, weight, node != notFound
-}
-
-func (e *matrixExplorer) explorePath(node []int) []int {
-	var exploration = make([]int, len(node))
-	for i := 0; i < len(node); i++ {
-		if node[i] != uncovered {
-			exploration[i] = node[i]
-			continue
-		}
-		exploration[i] = infinity
-	}
-	return exploration
+	return node, node > -1
 }
 
 func New(geo [][]int) *matrixGraph {
--- dijkstra/matrix_test.go
+++ dijkstra/matrix_test.go
@@ -27,30 +27,42 @@ var testData = [][]int{
 
 func TestPathFinder_GetPath(t *testing.T) {
 	var pFinder = New(testData)
-	if cost, path := pFinder.GetPath(0, 10); cost != 70 {
-		t.Errorf("expected 70, got: %d", cost)
-	} else if !reflect.DeepEqual(path, []int{0, 1, 2, 5, 13, 12, 10}) {
-		t.Errorf("wrong path: %v", path)
-	}
-	if cost, path := pFinder.GetPath(0, 11); cost != 61 {
-		t.Errorf("expected 62, got: %d", cost)
-	} else if !reflect.DeepEqual(path, []int{0, 1, 2, 5, 13, 12, 11}) {
-		t.Errorf("wrong path: %v", path)
-	}
-	if cost, path := pFinder.GetPath(0, 4); cost != 36 {
-		t.Errorf("expected 36, got: %d", cost)
-	} else if !reflect.DeepEqual(path, []int{0, 1, 2, 3, 4}) {
-		t.Errorf("wrong path: %v", path)
-	}
-	if cost, _ := pFinder.GetPath(0, 0); cost != infinity {
-		t.Errorf("expected infinity, got: %d", cost)
-	}
-	if cost, _ := pFinder.GetPath(11, 8); cost != infinity {
-		t.Errorf("expected infinity, got: %d", cost)
-	}
-	if cost, _ := pFinder.GetPath(0, 18); cost != infinity {
-		t.Errorf("expected infinity, got: %d", cost)
-	}
+	t.Run("0_10", func(t *testing.T) {
+		if cost, path := pFinder.GetPath(0, 10); cost != 70 {
+			t.Errorf("expected 70, got: %d", cost)
+		} else if !reflect.DeepEqual(path, []int{0, 1, 2, 5, 13, 12, 10}) {
+			t.Errorf("wrong path: %v", path)
+		}
+	})
+	t.Run("0_11", func(t *testing.T) {
+		if cost, path := pFinder.GetPath(0, 11); cost != 61 {
+			t.Errorf("expected 62, got: %d", cost)
+		} else if !reflect.DeepEqual(path, []int{0, 1, 2, 5, 13, 12, 11}) {
+			t.Errorf("wrong path: %v", path)
+		}
+	})
+	t.Run("0_4", func(t *testing.T) {
+		if cost, path := pFinder.GetPath(0, 4); cost != 36 {
+			t.Errorf("expected 36, got: %d", cost)
+		} else if !reflect.DeepEqual(path, []int{0, 1, 2, 3, 4}) {
+			t.Errorf("wrong path: %v", path)
+		}
+	})
+	t.Run("0_0", func(t *testing.T) {
+		if cost, _ := pFinder.GetPath(0, 0); cost != infinity {
+			t.Errorf("expected infinity, got: %d", cost)
+		}
+	})
+	t.Run("11_8", func(t *testing.T) {
+		if cost, _ := pFinder.GetPath(11, 8); cost != infinity {
+			t.Errorf("expected infinity, got: %d", cost)
+		}
+	})
+	t.Run("0_18", func(t *testing.T) {
+		if cost, _ := pFinder.GetPath(0, 18); cost != infinity {
+			t.Errorf("expected infinity, got: %d", cost)
+		}
+	})
 }
 
 func Benchmark_PathFinder_GetPath(b *testing.B) {
@@ -93,7 +105,6 @@ func TestPathFinder_nextNode(t *testing.T) {
 		name   string
 		fields fields
 		args   args
-		want   int
 		want1  int
 		want2  bool
 	}{
@@ -101,23 +112,20 @@ func TestPathFinder_nextNode(t *testing.T) {
 			name:   "first",
 			fields: fields{explored: []bool{false, false, false, false}},
 			args:   args{exploration: []int{1, 2, 3, 4}},
-			want:   0,
-			want1:  1,
+			want1:  0,
 			want2:  true,
 		},
 		{
 			name:   "last",
 			fields: fields{explored: []bool{false, false, false, false}},
 			args:   args{exploration: []int{4, 3, 2, 1}},
-			want:   3,
-			want1:  1,
+			want1:  3,
 			want2:  true,
 		},
 		{
 			name:   "explored_1",
 			fields: fields{explored: []bool{false, false, false, true}},
 			args:   args{exploration: []int{4, 3, 2, 1}},
-			want:   2,
 			want1:  2,
 			want2:  true,
 		},
@@ -125,31 +133,26 @@ func TestPathFinder_nextNode(t *testing.T) {
 			name:   "explored_all",
 			fields: fields{explored: []bool{true, true, true, true}},
 			args:   args{exploration: []int{4, 3, 2, 1}},
-			want:   -1,
-			want1:  infinity,
+			want1:  -1,
 			want2:  false,
 		},
 		{
 			name:   "empty_slice",
 			fields: fields{explored: []bool{}},
 			args:   args{exploration: []int{}},
-			want:   -1,
-			want1:  infinity,
+			want1:  -1,
 			want2:  false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			p := &matrixExplorer{
-				explored: tt.fields.explored,
-				current:  tt.args.exploration,
+				explored:        tt.fields.explored,
+				exploredWeights: tt.args.exploration,
 			}
-			got, got1, got2 := p.nextNode()
-			if got != tt.want {
-				t.Errorf("nextNode() got = %v, want %v", got, tt.want)
-			}
+			got1, got2 := p.nextNode()
 			if got1 != tt.want1 {
-				t.Errorf("nextNode() got1 = %v, want %v", got1, tt.want1)
+				t.Errorf("nextNode() got = %v, want %v", got1, tt.want1)
 			}
 			if got2 != tt.want2 {
 				t.Errorf("nextNode() got2 = %v, want %v", got2, tt.want2)

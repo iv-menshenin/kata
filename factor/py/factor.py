1740c6e87198b8ed10c5fc39067ccc85984100e1
devalio <github@devalio.ru>
2023-02-10 07:38:28 +0300

    fixed "factor" for python (bad test case)

--- factor/py/factor.py
+++ factor/py/factor.py
@@ -1,12 +1,9 @@
 def factor(x):
-    if x == 1:
-        return [1]
     cur = 2
     arr = []
     while x > 1:
-        if x % cur == 0:
+        while x % cur == 0:
             arr.append(cur)
             x = x / cur
-        else:
-            cur = cur + 1
+        cur = cur + 1
     return arr
--- factor/py/factor_test.py
+++ factor/py/factor_test.py
@@ -6,7 +6,7 @@ from factor import factor
 class TestFactor(unittest.TestCase):
 
     def test_factor(self):
-        self.assertEqual(factor(1), [1])
+        self.assertEqual(factor(1), [])
         self.assertEqual(factor(2), [2])
         self.assertEqual(factor(3), [3])
         self.assertEqual(factor(4), [2, 2])

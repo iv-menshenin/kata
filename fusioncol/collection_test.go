78a9de601ae0893573eb4023acd82fb71315732d
devalio <github@devalio.ru>
2024-03-02 10:16:43 +0300

    fusion collection: more test

--- fusioncol/collection_test.go
+++ fusioncol/collection_test.go
@@ -9,23 +9,50 @@ import (
 
 func TestFusionCollection(t *testing.T) {
 	t.Parallel()
-	type Elem struct {
-		i int
-		s string
-	}
-	var c Collection[Elem]
+	t.Run("append_get", func(t *testing.T) {
+		t.Parallel()
 
-	const elemCount = 100000
+		var c Collection[int64]
+		c.Append(101)
+		c.Append(102)
+		c.Append(103)
+		require.Equal(t, int64(103), *c.Get(2))
+		require.Equal(t, int64(102), *c.Get(1))
+		require.Equal(t, int64(101), *c.Get(0))
+	})
+	t.Run("len", func(t *testing.T) {
+		t.Parallel()
 
-	for n := 0; n < elemCount; n++ {
-		c.Append(Elem{i: n, s: strconv.Itoa(n)})
-	}
+		var c Collection[int64]
+		require.Equal(t, 0, c.Len())
+		c.Append(101)
+		require.Equal(t, 1, c.Len())
+		c.Append(102)
+		require.Equal(t, 2, c.Len())
+		c.Append(103)
+		require.Equal(t, 3, c.Len())
+	})
+	t.Run("force", func(t *testing.T) {
+		t.Parallel()
+		type Elem struct {
+			i int
+			s string
+		}
+		var c Collection[Elem]
 
-	for n := 0; n < elemCount; n++ {
-		e := c.Get(n)
-		require.Equal(t, n, e.i)
-		require.Equal(t, strconv.Itoa(n), e.s)
-	}
+		const elemCount = 100000
+
+		for n := 0; n < elemCount; n++ {
+			require.Equal(t, n, c.Len())
+			c.Append(Elem{i: n, s: strconv.Itoa(n)})
+		}
+
+		for n := 0; n < elemCount; n++ {
+			e := c.Get(n)
+			require.Equal(t, n, e.i)
+			require.Equal(t, strconv.Itoa(n), e.s)
+		}
+	})
 }
 
 func TestFusionCollectionPushPop(t *testing.T) {
@@ -35,9 +62,13 @@ func TestFusionCollectionPushPop(t *testing.T) {
 		var c Collection[string]
 
 		c.Push("foo")
+		require.Equal(t, 1, c.Len())
 		c.Push("bar")
+		require.Equal(t, 2, c.Len())
 		require.Equal(t, "bar", c.Pop())
+		require.Equal(t, 1, c.Len())
 		require.Equal(t, "foo", c.Pop())
+		require.Equal(t, 0, c.Len())
 
 		c.Push("1")
 		c.Push("3")
@@ -45,12 +76,14 @@ func TestFusionCollectionPushPop(t *testing.T) {
 		c.Push("2")
 		c.Push("3")
 		c.Push("4")
+		require.Equal(t, 4, c.Len())
 		require.Equal(t, "4", c.Pop())
 		require.Equal(t, "3", c.Pop())
 		require.Equal(t, "2", c.Pop())
 		require.Equal(t, "1", c.Pop())
+		require.Equal(t, 0, c.Len())
 	})
-	t.Run("buff_overflow", func(t *testing.T) {
+	t.Run("force", func(t *testing.T) {
 		t.Parallel()
 
 		var c Collection[int]
